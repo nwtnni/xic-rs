8130223719bcfadc46b30fb4793be04b1e39d1d9
Newton Ni <nwtnni@gmail.com>
2019-01-28 17:13:44 -0500

    Implement span data types

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
+mod error;
 mod lex;
+mod span;
 mod token;
---
+++ src/span.rs
@@ -0,0 +1,23 @@
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Point {
+    pub row: usize,
+    pub col: usize,
+}
+
+impl std::fmt::Display for Point {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(fmt, "{}:{}", self.row, self.col)
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Span {
+    pub lo: Point,
+    pub hi: Point,
+}
+
+impl std::fmt::Display for Span {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(fmt, "{}-{}", self.lo, self.hi)
+    }
+}
