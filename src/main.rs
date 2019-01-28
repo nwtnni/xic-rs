f0065422883d05be5f135e964e5c6f8cd0970543
Newton Ni <nwtnni@gmail.com>
2019-01-28 17:27:19 -0500

    Implement CLI using structopt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "xic", about = "Compiler for the Xi programming language.")]
+struct Arguments {
+    /// Generate output from lexical analysis
+    #[structopt(short = "l", long = "lex")]
+    lex_output: bool,  
+
+    /// Specify where to place generated diagnostic files
+    #[structopt(short = "D", parse(from_os_str))]
+    output_dir: Option<std::path::PathBuf>,
+
+    /// Source files to compile
+    #[structopt(parse(from_os_str))]
+    files: Vec<std::path::PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::from_args();
+    println!("{:?}", args);
 }
