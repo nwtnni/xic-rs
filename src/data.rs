76684c6131225c3986b109a244c9e73bebd06c62
Newton Ni <nwtnni@gmail.com>
2019-03-05 18:59:57 -0500

    Start implementing operands

--- src/data.rs
+++ src/data.rs
@@ -1,3 +1,4 @@
 pub mod token;
 pub mod ast;
 pub mod typ;
+pub mod operand;
---
+++ src/data/operand.rs
@@ -0,0 +1,66 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+use crate::util::symbol;
+
+static LABELS: AtomicUsize = AtomicUsize::new(0);
+static TEMPS: AtomicUsize = AtomicUsize::new(0);
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Imm {
+    Const(i64),
+    Label(Label),
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Label {
+    Fix(symbol::Symbol),
+    Gen(symbol::Symbol, usize),
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Temp {
+    Reg(Reg),
+    Gen(symbol::Symbol, usize),
+}
+
+impl std::fmt::Display for Temp {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        unimplemented!()
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Reg {
+    RAX,
+    RBX,
+    RCX,
+    RDX,
+    RBP,
+    RSP,
+    RSI,
+    RDI,
+    R8,
+    R9,
+    R10,
+    R11,
+    R12,
+    R13,
+    R14,
+    R15,
+}
+
+impl std::fmt::Display for Reg {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        unimplemented!()
+    }
+}
+
+pub trait Operand: Copy + Eq + std::hash::Hash + std::fmt::Display + std::fmt::Debug {}
+impl Operand for Temp {} 
+impl Operand for Reg {}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Mem<T: Operand> {
+    R(T),
+    RO(T, i32),
+}
