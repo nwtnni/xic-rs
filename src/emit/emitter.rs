97c66876b78147321277601aaf38041d35bf43c8
Newton Ni <nwtnni@gmail.com>
2019-03-11 18:11:41 -0400

    Use vec macro for array indexing bounds checks

--- src/emit/emitter.rs
+++ src/emit/emitter.rs
@@ -179,17 +179,15 @@ impl Emitter {
             let hi = operand::Label::new("HIGH_BOUND");
             let fail = operand::Label::Fix(symbol::intern(XI_OUT_OF_BOUNDS));
 
-            let mut seq = Vec::new();
-            seq.push(hir::Stm::Move(index.clone(), self.emit_exp(&*idx, vars).into()));
-            seq.push(hir::Stm::CJump(ir::Rel::Ge, index.clone(), hir::Exp::Int(0), lo, fail));
-            seq.push(hir::Stm::Label(lo));
-            seq.push(hir::Stm::CJump(ir::Rel::Lt, index.clone(), memory.clone(), hi, fail));
-            seq.push(hir::Stm::Label(hi));
+            let bounds = hir::Stm::Seq(vec![
+                hir::Stm::Move(index.clone(), self.emit_exp(&*idx, vars).into()),
+                hir::Stm::CJump(ir::Rel::Lt, index.clone(), hir::Exp::Int(0), fail, lo),
+                hir::Stm::Label(lo),
+                hir::Stm::CJump(ir::Rel::Ge, index.clone(), memory.clone(), fail, hi),
+                hir::Stm::Label(hi),
+            ]);
 
-            hir::Exp::ESeq(
-                Box::new(hir::Stm::Seq(seq)),
-                Box::new(offset)
-            ).into()
+            hir::Exp::ESeq(Box::new(bounds), Box::new(offset)).into()
         }
         | Call(ast::Call { name, args, ..}) if *name == symbol::intern("length") => {
             let address = self.emit_exp(&args[0], vars).into();
