79dacc9cd3e747b8caf40ebac471ab3d64ae855c
Newton Ni <nwtnni@gmail.com>
2022-04-26 12:56:08 -0400

    Add tests for passing LIR through CFG

--- tests/emit.rs
+++ tests/emit.rs
@@ -40,3 +40,25 @@ pub fn interpret(path: &str) {
     pretty_assertions::assert_eq!(hir_stdout, lir_stdout);
     insta::assert_snapshot!(path, lir_stdout);
 }
+
+#[test_generator::test_resources("tests/interpret/*.xi")]
+pub fn reorder(path: &str) {
+    let hir = compile(path);
+    let lir = xic::api::emit_lir(&hir);
+
+    let mut lir_stdin = io::Cursor::new(Vec::new());
+    let mut lir_stdout = io::Cursor::new(Vec::new());
+    xic::api::interpret_lir(&lir, &mut lir_stdin, &mut lir_stdout).unwrap();
+
+    let cfg = xic::api::construct_control_flow(&lir);
+    let cfg = xic::api::destruct_control_flow(&cfg);
+
+    let mut cfg_stdin = io::Cursor::new(Vec::new());
+    let mut cfg_stdout = io::Cursor::new(Vec::new());
+    xic::api::interpret_lir(&cfg, &mut cfg_stdin, &mut cfg_stdout).unwrap();
+
+    let lir_stdout = String::from_utf8(lir_stdout.into_inner()).unwrap();
+    let cfg_stdout = String::from_utf8(cfg_stdout.into_inner()).unwrap();
+
+    pretty_assertions::assert_eq!(lir_stdout, cfg_stdout);
+}
