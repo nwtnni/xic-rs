35a39fc8546f4d452212b7a71190fc1f225fc460
Newton Ni <nwtnni@gmail.com>
2022-04-27 11:05:36 -0400

    Implement assembly data types

---
+++ src/data/asm.rs
@@ -0,0 +1,61 @@
+use crate::data::operand;
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Assembly<T> {
+    Binary(Binary, operand::Two<T>),
+    Unary(Unary, operand::One<T>),
+    Nullary(Nullary),
+    Directive(Directive),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Directive {
+    Align(usize),
+    Local(operand::Label),
+    Global(operand::Label),
+    Quad(Vec<i64>),
+    Data,
+    Text,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Binary {
+    Add,
+    Sub,
+    And,
+    Or,
+    Xor,
+    Cmp,
+    Mov,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Unary {
+    Inc,
+    Dec,
+    Not,
+    Neg,
+    Push,
+    Pop,
+    Call,
+    Mul,
+    Div,
+    Jmp,
+    Jcc(Condition),
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Nullary {
+    Cqo,
+    Ret,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Condition {
+    E,
+    Ne,
+    G,
+    Ge,
+    L,
+    Le,
+}
--- src/data/operand.rs
+++ src/data/operand.rs
@@ -66,7 +66,76 @@ impl Operand for Temporary {}
 impl Operand for Register {}
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
-pub enum Memory<T: Operand> {
+pub enum Memory<T> {
+    B {
+        base: T,
+    },
+    BI {
+        base: T,
+        index: T,
+    },
+    BO {
+        base: T,
+        offset: Immediate,
+    },
+    BIO {
+        base: T,
+        index: T,
+        offset: Immediate,
+    },
+    BIS {
+        base: T,
+        index: T,
+        scale: Scale,
+    },
+    ISO {
+        index: T,
+        scale: Scale,
+        offset: Immediate,
+    },
+    BISO {
+        base: T,
+        index: T,
+        scale: Scale,
+        offset: Immediate,
+    },
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum Scale {
+    _1,
+    _2,
+    _4,
+    _8,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum Two<T> {
+    RI {
+        destination: T,
+        source: Immediate,
+    },
+    MI {
+        destination: Memory<T>,
+        source: Immediate,
+    },
+    MR {
+        destination: Memory<T>,
+        source: T,
+    },
+    RM {
+        destination: T,
+        source: Memory<T>,
+    },
+    RR {
+        destination: T,
+        source: T,
+    },
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum One<T> {
+    I(Immediate),
     R(T),
-    RO(T, i32),
+    M(Memory<T>),
 }
