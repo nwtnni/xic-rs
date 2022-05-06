a1b2671c3dfe31ef663aee56b6a16d99671011f5
Newton Ni <nwtnni@gmail.com>
2022-05-06 16:24:14 -0400

    Move tests under a single suite
    For enabling code reuse across submodules.

---
+++ tests/suite.rs
@@ -0,0 +1,20 @@
+#[path = "suite/lex.rs"]
+mod lex;
+
+#[path = "suite/parse.rs"]
+mod parse;
+
+#[path = "suite/check.rs"]
+mod check;
+
+#[path = "suite/emit.rs"]
+mod emit;
+
+#[path = "suite/assemble.rs"]
+mod assemble;
+
+#[path = "suite/analyze.rs"]
+mod analyze;
+
+#[path = "suite/optimize.rs"]
+mod optimize;
