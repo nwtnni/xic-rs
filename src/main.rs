39e8eff8263953a92b5618167927ffa80a833b19
Newton Ni <nwtnni@gmail.com>
2022-05-07 11:55:36 -0400

    Overhaul command line interface
    - Switch to `clap` from `structopt`
    - Factor out common functionality into methods on `Command`
      (e.g. checking for flags, writing to debug output)
    - Allow redirecting debug and assembly output to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ffi::OsStr;
+use std::fmt;
 use std::fs;
 use std::io;
 use std::io::Write as _;
@@ -6,82 +8,329 @@ use std::path::PathBuf;
 use std::str;
 
 use anyhow::anyhow;
-use structopt::StructOpt;
+use anyhow::Context as _;
+use clap::Parser;
+use clap::ValueHint;
+use xic::api;
+use xic::api::analyze;
+use xic::api::optimize;
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "xic", about = "Compiler for the Xi programming language.")]
+#[derive(Debug, Parser)]
+#[clap(name = "xic", about = "Compiler for the Xi programming language.")]
 struct Command {
+    /// Directory to search for source files
+    #[clap(
+        long = "sourcepath",
+        default_value = ".",
+        value_hint = ValueHint::DirPath,
+        display_order = 0,
+    )]
+    directory_source: PathBuf,
+
+    /// Directory to search for library files
+    ///
+    /// Defaults to the same directory as the source file using the library.
+    #[clap(
+        long = "libpath",
+        value_hint = ValueHint::DirPath,
+        display_order = 1,
+    )]
+    directory_library: Option<PathBuf>,
+
+    /// Directory to place generated diagnostic files
+    ///
+    /// If provided with `-`, output to `stdout`.
+    #[clap(
+        short = 'D',
+        default_value = ".",
+        value_hint = ValueHint::DirPath,
+        display_order = 2,
+    )]
+    directory_debug: PathBuf,
+
+    /// Directory to place final assembly files
+    ///
+    /// If provided with `-`, output to `stdout`.
+    #[clap(
+        short = 'd',
+        default_value = ".",
+        value_hint = ValueHint::DirPath,
+        display_order = 3,
+    )]
+    directory_output: PathBuf,
+
+    /// Enable optimizations
+    ///
+    /// Takes a comma-separated list of optimzations to enable.
+    #[clap(
+        short = 'O',
+        long = "O",
+        id = "optimize-enable",
+        conflicts_with = "optimize-disable",
+        use_value_delimiter = true,
+        require_value_delimiter = true,
+        multiple_occurrences = false,
+        min_values = 0,
+        value_name = "OPTIMIZATION",
+        possible_values = OPTIMIZATIONS,
+        display_order = 4,
+    )]
+    optimize_enable: Option<Vec<Opt>>,
+
+    /// Disable optimizations
+    ///
+    /// Takes a comma-separated list of optimzations to disable.
+    #[clap(
+        short = 'o',
+        long = "O-no",
+        id = "optimize-disable",
+        conflicts_with = "optimize-enable",
+        use_value_delimiter = true,
+        require_value_delimiter = true,
+        multiple_occurrences = false,
+        min_values = 1,
+        value_name = "OPTIMIZATION",
+        possible_values = OPTIMIZATIONS,
+        display_order = 5,
+    )]
+    optimize_disable: Option<Vec<Opt>>,
+
     /// Generate output from lexical analysis
-    #[structopt(short = "l", long = "lex")]
+    #[clap(short = 'l', long = "lex", display_order = 6)]
     debug_lex: bool,
 
     /// Generate output from syntactic analysis
-    #[structopt(short = "p", long = "parse")]
+    #[clap(short = 'p', long = "parse", display_order = 7)]
     debug_parse: bool,
 
     /// Generate output from semantic analysis
-    #[structopt(short = "t", long = "typecheck")]
+    #[clap(short = 't', long = "typecheck", display_order = 8)]
     debug_check: bool,
 
-    /// Generate output from emitted IR
-    #[structopt(short = "g", long = "irgen")]
-    debug_ir: bool,
+    /// Generate output from emitted IR, before canonization, reordering, and CFG optimization
+    #[clap(short = 'h', long = "hirgen", display_order = 9)]
+    debug_hir: bool,
 
-    /// Generate DOT file from IR CFG
-    #[structopt(short = "c", long = "optcfg")]
-    debug_cfg: bool,
+    /// Generate IR CFG output in DOT format after optimization phases
+    ///
+    /// Takes a comma-separated list of optimization names.
+    #[clap(
+        long = "optir",
+        use_value_delimiter = true,
+        require_value_delimiter = true,
+        value_name = "OPTIMIZATION",
+        possible_values = [
+            DebugOpt::Initial.to_static_str(),
+            DebugOpt::Final.to_static_str(),
+        ],
+        display_order = 10,
+    )]
+    debug_optimize_lir: Vec<DebugOpt>,
 
-    /// Generate output from abstract assembly
-    #[structopt(short = "a", long = "abstract-assembly")]
-    debug_abstract_assembly: bool,
+    /// Generate output from emitted IR, after canonization, reordering, and CFG optimization
+    #[clap(short = 'g', long = "irgen", alias = "lirgen", display_order = 11)]
+    debug_lir: bool,
 
     /// Interpret emitted IR
-    #[structopt(short = "r", long = "irrun")]
+    #[clap(short = 'r', long = "irrun", display_order = 12)]
     interpret_ir: bool,
 
-    /// Enable optimizations
-    #[structopt(short = "O")]
-    optimize: Option<Vec<Optimization>>,
+    /// Generate output from abstract assembly, before CFG optimization
+    #[clap(short = 'a', long = "tile", display_order = 13)]
+    debug_assembly: bool,
 
-    /// Specify where to place generated diagnostic files
-    #[structopt(short = "D", parse(from_os_str), default_value = ".")]
-    directory_debug: PathBuf,
+    /// Generate abstract assembly CFG in DOT format after optimization phases
+    ///
+    /// Takes a comma-separated list of optimization names.
+    #[clap(
+        long = "optcfg",
+        use_value_delimiter = true,
+        require_value_delimiter = true,
+        value_name = "OPTIMIZATION",
+        possible_values = [
+            DebugOpt::Initial.to_static_str(),
+            Opt::CopyPropagation.to_static_str(),
+            Opt::DeadCodeElimination.to_static_str(),
+            Opt::RegisterAllocation.to_static_str(),
+            DebugOpt::Final.to_static_str(),
+        ],
+        display_order = 14,
+    )]
+    debug_optimize_assembly: Vec<DebugOpt>,
 
-    /// Specify where to place generated assembly files
-    #[structopt(short = "d", parse(from_os_str), default_value = ".")]
-    directory_output: PathBuf,
-
-    /// Specify where to search for source files
-    #[structopt(long = "sourcepath", parse(from_os_str), default_value = ".")]
-    directory_source: PathBuf,
-
-    /// Specify where to search for library files
-    #[structopt(long = "libpath", parse(from_os_str))]
-    directory_library: Option<PathBuf>,
+    /// Print a newline-separated list of supported optimizations
+    #[clap(long = "report-opts", display_order = 15)]
+    report_optimizations: bool,
 
     /// Source files to compile, relative to `source_dir`
-    #[structopt(parse(from_os_str))]
+    #[clap(value_hint = ValueHint::FilePath)]
     input: Vec<PathBuf>,
 }
 
+impl Command {
+    fn optimize(&self, optimization: Opt) -> bool {
+        match (
+            self.optimize_enable.as_ref(),
+            self.optimize_disable.as_ref(),
+        ) {
+            (None, None) => true,
+            (None, Some(disable)) => !disable.contains(&optimization),
+            (Some(enable), None) => enable.contains(&optimization),
+            (Some(_), Some(_)) => unreachable!("Mutual exclusivity guaranteed by `clap`"),
+        }
+    }
+
+    fn debug_optimize_lir<T: fmt::Display, O: Into<DebugOpt>>(
+        &self,
+        path: &Path,
+        optimization: O,
+        data: T,
+    ) -> anyhow::Result<()> {
+        let optimization = optimization.into();
+
+        if !self.debug_optimize_lir.contains(&optimization) {
+            return Ok(());
+        }
+
+        self.debug_optimize(path, optimization, data)
+    }
+
+    fn debug_optimize_assembly<T: fmt::Display, O: Into<DebugOpt>>(
+        &self,
+        path: &Path,
+        optimization: O,
+        data: T,
+    ) -> anyhow::Result<()> {
+        let optimization = optimization.into();
+
+        if !self.debug_optimize_assembly.contains(&optimization) {
+            return Ok(());
+        }
+
+        self.debug_optimize(path, optimization, data)
+    }
+
+    fn debug_optimize<T: fmt::Display>(
+        &self,
+        path: &Path,
+        optimization: DebugOpt,
+        data: T,
+    ) -> anyhow::Result<()> {
+        // https://github.com/rust-lang/rust/issues/86319
+        let mut file_stem = path
+            .file_stem()
+            .map(OsStr::to_os_string)
+            .ok_or_else(|| anyhow!("Expected .xi file, but got {}", path.display()))?;
+
+        file_stem.push("_");
+        file_stem.push(optimization.to_static_str());
+
+        let path = path.with_file_name(file_stem);
+
+        self.debug(&path, "dot", data)
+    }
+
+    fn debug<T: fmt::Display>(&self, path: &Path, extension: &str, data: T) -> anyhow::Result<()> {
+        if self.directory_debug == Path::new("-") {
+            println!("{}", data);
+            return Ok(());
+        }
+
+        self.write(&self.directory_debug.join(path), extension, data)
+    }
+
+    fn output<T: fmt::Display>(&self, path: &Path, extension: &str, data: T) -> anyhow::Result<()> {
+        if self.directory_output == Path::new("-") {
+            println!("{}", data);
+            return Ok(());
+        }
+
+        self.write(&self.directory_output.join(path), extension, data)
+    }
+
+    fn write<T: fmt::Display>(&self, path: &Path, extension: &str, data: T) -> anyhow::Result<()> {
+        let path = path.with_extension(extension);
+        let mut file = fs::File::create(&path)
+            .map(io::BufWriter::new)
+            .with_context(|| anyhow!("Failed to create file: {}", path.display()))?;
+        write!(file, "{}", data)
+            .with_context(|| anyhow!("Failed to write to file: {}", path.display()))?;
+        Ok(())
+    }
+}
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
-enum Optimization {
+enum DebugOpt {
+    Initial,
+    Opt(Opt),
+    Final,
+}
+
+impl From<Opt> for DebugOpt {
+    fn from(optimization: Opt) -> Self {
+        Self::Opt(optimization)
+    }
+}
+
+impl DebugOpt {
+    fn to_static_str(self) -> &'static str {
+        match self {
+            DebugOpt::Initial => "initial",
+            DebugOpt::Opt(optimization) => optimization.to_static_str(),
+            DebugOpt::Final => "final",
+        }
+    }
+}
+
+impl str::FromStr for DebugOpt {
+    type Err = anyhow::Error;
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        match string {
+            "initial" => Ok(DebugOpt::Initial),
+            "final" => Ok(DebugOpt::Final),
+            _ => Opt::from_str(string).map(DebugOpt::Opt),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum Opt {
     ConstantFold,
+    CopyPropagation,
     DeadCodeElimination,
     RegisterAllocation,
 }
 
-impl str::FromStr for Optimization {
+// Need something like https://doc.rust-lang.org/std/mem/fn.variant_count.html
+// to make sure array matches up with enum definition. Procedural macro options
+// seem too heavyweight for something like this.
+const OPTIMIZATIONS: [&str; 4] = [
+    Opt::ConstantFold.to_static_str(),
+    Opt::CopyPropagation.to_static_str(),
+    Opt::DeadCodeElimination.to_static_str(),
+    Opt::RegisterAllocation.to_static_str(),
+];
+
+impl Opt {
+    const fn to_static_str(self) -> &'static str {
+        match self {
+            Opt::ConstantFold => "cf",
+            Opt::CopyPropagation => "copy",
+            Opt::DeadCodeElimination => "dce",
+            Opt::RegisterAllocation => "reg",
+        }
+    }
+}
+
+impl str::FromStr for Opt {
     type Err = anyhow::Error;
     fn from_str(string: &str) -> Result<Self, Self::Err> {
         match string {
-            "cf" => Ok(Optimization::ConstantFold),
-            "dec" => Ok(Optimization::DeadCodeElimination),
-            "reg" => Ok(Optimization::RegisterAllocation),
-            _ => Err(anyhow!(
-                "Unknown optimization {}, expected one of [cf, reg]",
-                string
-            )),
+            "cf" => Ok(Opt::ConstantFold),
+            "copy" => Ok(Opt::CopyPropagation),
+            "dce" => Ok(Opt::DeadCodeElimination),
+            "reg" => Ok(Opt::RegisterAllocation),
+            _ => Err(anyhow!("Unknown optimization {}, ", string)),
         }
     }
 }
@@ -89,32 +338,31 @@ impl str::FromStr for Optimization {
 fn main() -> anyhow::Result<()> {
     pretty_env_logger::init_timed();
 
-    let command = Command::from_args();
+    let command = Command::parse();
+
+    if command.report_optimizations {
+        for optimization in OPTIMIZATIONS {
+            println!("{}", optimization);
+        }
+        return Ok(());
+    }
 
     for path in &command.input {
         let path = command.directory_source.join(path);
 
-        let tokens = xic::api::lex(&path)?;
+        let tokens = api::lex(&path)?;
 
         if command.debug_lex {
-            write!(
-                debug(&command.directory_debug, &path, "lexed")?,
-                "{}",
-                tokens
-            )?;
+            command.debug(&path, "lexed", &tokens)?;
         }
 
-        let program = xic::api::parse(tokens)?;
+        let program = api::parse(tokens)?;
 
         if command.debug_parse {
-            write!(
-                debug(&command.directory_debug, &path, "parsed")?,
-                "{}",
-                program,
-            )?;
+            command.debug(&path, "parsed", &program)?;
         }
 
-        let context = xic::api::check(
+        let context = api::check(
             command
                 .directory_library
                 .as_deref()
@@ -124,91 +372,88 @@ fn main() -> anyhow::Result<()> {
         );
 
         if command.debug_check {
-            let mut file = debug(&command.directory_debug, &path, "typed")?;
-            match &context {
-                Ok(_) => write!(file, "Valid Xi Program")?,
-                Err(error) => write!(file, "{}", error)?,
-            }
-        }
-
-        let mut hir = xic::api::emit_hir(&path, &program, &context?);
-
-        if command.optimize.as_ref().map_or(true, |optimizations| {
-            optimizations.contains(&Optimization::ConstantFold)
-        }) {
-            hir = hir.map(xic::api::optimize::constant_fold);
-        }
-
-        if command.debug_ir {
-            write!(debug(&command.directory_debug, &path, "hir")?, "{}", hir,)?;
-        }
-
-        let mut lir = hir.map_ref(xic::api::emit_lir);
-
-        if command.optimize.as_ref().map_or(true, |optimizations| {
-            optimizations.contains(&Optimization::ConstantFold)
-        }) {
-            lir = lir.map(xic::api::optimize::constant_fold);
-        }
-
-        let cfg = lir.map(xic::api::construct_cfg);
-
-        if command.debug_cfg {
-            write!(debug(&command.directory_debug, &path, "dot")?, "{}", cfg)?;
-        }
-
-        let lir = cfg.map(xic::api::destruct_cfg);
-
-        if command.debug_ir {
-            write!(debug(&command.directory_debug, &path, "lir")?, "{}", lir)?;
-        }
-
-        if command.interpret_ir {
-            xic::api::interpret_lir(&lir, io::BufReader::new(io::stdin()), io::stdout())?;
-        }
-
-        let abstract_assembly = lir.map_ref(xic::api::tile);
-
-        if command.debug_abstract_assembly {
-            write!(
-                debug(&command.directory_debug, &path, "tiled")?,
-                "{}",
-                abstract_assembly.intel(),
+            command.debug(
+                &path,
+                "typed",
+                match &context {
+                    Ok(_) => String::from("Valid Xi Program"),
+                    Err(error) => error.to_string(),
+                },
             )?;
         }
 
-        let assembly = if command.optimize.as_ref().map_or(true, |optimizations| {
-            optimizations.contains(&Optimization::RegisterAllocation)
-        }) {
-            abstract_assembly
-                .map(xic::api::construct_cfg)
-                .map(xic::api::allocate_linear)
-        } else if command.optimize.as_ref().map_or(true, |optimizations| {
-            optimizations.contains(&Optimization::DeadCodeElimination)
-        }) {
-            abstract_assembly
-                .map(xic::api::construct_cfg)
-                .map(|mut cfg| {
-                    let live_variables = xic::api::analyze::analyze(&cfg);
-                    xic::api::optimize::eliminate_dead_code(&live_variables, &mut cfg);
-                    cfg
-                })
-                .map(xic::api::destruct_cfg)
-                .map_ref(xic::api::allocate_trivial)
+        let mut hir = api::emit_hir(&path, &program, &context?);
+
+        if command.optimize(Opt::ConstantFold) {
+            hir = hir.map(optimize::constant_fold);
+        }
+
+        if command.debug_hir {
+            command.debug(&path, "hir", &hir)?;
+        }
+
+        let mut lir = hir.map_ref(api::emit_lir);
+
+        if command.optimize(Opt::ConstantFold) {
+            lir = lir.map(optimize::constant_fold);
+        }
+
+        let cfg = lir.map(api::construct_cfg);
+
+        command.debug_optimize_lir(&path, DebugOpt::Initial, &cfg)?;
+        command.debug_optimize_lir(&path, DebugOpt::Final, &cfg)?;
+
+        let lir = cfg.map(api::destruct_cfg);
+
+        if command.debug_lir {
+            command.debug(&path, "lir", &lir)?;
+        }
+
+        if command.interpret_ir {
+            api::interpret_lir(&lir, io::BufReader::new(io::stdin()), io::stdout())?;
+        }
+
+        let abstract_assembly = lir.map_ref(api::tile);
+
+        if command.debug_assembly {
+            command.debug(&path, "tiled", &abstract_assembly)?;
+        }
+
+        let mut cfg = abstract_assembly.map(api::construct_cfg);
+
+        command.debug_optimize_assembly(&path, DebugOpt::Initial, &cfg)?;
+
+        if command.optimize(Opt::CopyPropagation) {
+            cfg.map_mut(optimize::copy_propagate);
+            command.debug_optimize_assembly(&path, Opt::CopyPropagation, &cfg)?;
+        }
+
+        // Register allocation implies dead code elimination, so we don't want to
+        // run it redundantly if the user doesn't want debug output for it.
+        if command.optimize(Opt::DeadCodeElimination)
+            && (!command.optimize(Opt::RegisterAllocation)
+                || command
+                    .debug_optimize_assembly
+                    .contains(&DebugOpt::Opt(Opt::DeadCodeElimination)))
+        {
+            cfg.map_mut(|function| {
+                let live_variables = analyze::analyze(function);
+                optimize::eliminate_dead_code(&live_variables, function);
+            });
+
+            command.debug_optimize_assembly(&path, Opt::DeadCodeElimination, &cfg)?;
+        }
+
+        command.debug_optimize_assembly(&path, DebugOpt::Final, &cfg)?;
+
+        let assembly = if command.optimize(Opt::RegisterAllocation) {
+            cfg.map(api::allocate_linear)
         } else {
-            abstract_assembly.map_ref(xic::api::allocate_trivial)
+            cfg.map(api::destruct_cfg).map_ref(api::allocate_trivial)
         };
 
-        write!(
-            debug(&command.directory_output, &path, "S")?,
-            "{}",
-            assembly.intel(),
-        )?;
+        command.output(&path, "S", assembly.intel())?;
     }
 
     Ok(())
-}
-
-fn debug(directory: &Path, path: &Path, extension: &str) -> io::Result<io::BufWriter<fs::File>> {
-    fs::File::create(directory.join(path).with_extension(extension)).map(io::BufWriter::new)
 }
