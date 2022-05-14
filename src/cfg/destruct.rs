1514d5614660394a7ae18889156cef3c03c6b7ba
Newton Ni <nwtnni@gmail.com>
2022-05-14 16:37:21 -0400

    Fix fallthrough to exit bug in CFG destruction
    We need to check that the fallthrough case isn't the exit, which we
    forcibly place at the end of the linear sequence. Also omit
    unnecessary jump instructions when transforming out of the CFG--they
    are reintroduced when constructing the CFG so that analyses can modify
    or inspect them if necessary.

--- src/cfg/destruct.rs
+++ src/cfg/destruct.rs
@@ -1,6 +1,7 @@
 use crate::cfg::Cfg;
 use crate::cfg::Edge;
 use crate::cfg::Function;
+use crate::cfg::Terminator;
 
 /// After linearization, guarantees that `function.enter` is at the beginning of the
 /// list, and that `function.exit` is at the end. This property is useful for tiling
@@ -27,13 +28,37 @@ pub fn destruct_cfg<T: Function>(mut function: Cfg<T>) -> T::Fallthrough {
         let mut conditional = [None; 2];
 
         for (_, next, edge) in function.graph.edges(label) {
+            // Special case: since we require that the exit block is the
+            // last statement in the linearized function, we can't fall
+            // through to it unless `label` is the penultimate block.
             match edge {
-                Edge::Unconditional => dfs.push(next),
-                Edge::Conditional(true) => conditional[0] = Some(next),
-                Edge::Conditional(false) if function.blocks.contains_key(&next) => {
-                    conditional[1] = Some(next)
+                Edge::Unconditional if !function.blocks.contains_key(&next) => (),
+                Edge::Unconditional if next == function.exit && function.blocks.len() != 1 => {
+                    dfs.push(next);
                 }
-                Edge::Conditional(false) => statements.push(T::jump(next)),
+                Edge::Unconditional => {
+                    assert!(matches!(
+                        statements
+                            .pop()
+                            .and_then(|statement| T::to_terminator(&statement)),
+                        Some(Terminator::Jump(_))
+                    ));
+                    dfs.push(next);
+                }
+
+                Edge::Conditional(true) if !function.blocks.contains_key(&next) => (),
+                Edge::Conditional(true) => conditional[0] = Some(next),
+
+                Edge::Conditional(false) if !function.blocks.contains_key(&next) => {
+                    statements.push(T::jump(next));
+                }
+                Edge::Conditional(false) if next != function.exit || function.blocks.len() == 1 => {
+                    conditional[1] = Some(next);
+                }
+                Edge::Conditional(false) => {
+                    statements.push(T::jump(next));
+                    conditional[1] = Some(next);
+                }
             }
         }
 
