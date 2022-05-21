a1acaf7a10033e07aaaf2f0f1331230865d01cd5
Newton Ni <nwtnni@gmail.com>
2022-05-21 17:01:37 -0400

    Implement mangling methods for classes and globals

--- src/abi.rs
+++ src/abi.rs
@@ -38,6 +38,7 @@ use crate::data::operand::Register;
 use crate::data::operand::Temporary;
 use crate::data::operand::Unary;
 use crate::data::r#type;
+use crate::data::symbol;
 
 pub const WORD: i64 = 8;
 
@@ -169,12 +170,33 @@ pub fn write_return(address: Option<Temporary>, index: usize) -> Unary<Temporary
     })
 }
 
+#[allow(unused)]
+pub fn mangle_global(name: &str, r#type: &r#type::Expression) -> String {
+    let mut mangled = format!("_I_g_{}_", escape(name));
+    mangled.push('_');
+    mangle_type(r#type, &mut mangled);
+    mangled
+}
+
+#[allow(unused)]
+pub fn mangle_method(
+    class: &str,
+    name: &str,
+    parameters: &[r#type::Expression],
+    returns: &[r#type::Expression],
+) -> String {
+    let mut mangled = mangle_function(name, parameters, returns);
+    write!(mangled, "_M_").unwrap();
+    write!(mangled, "{}", escape(class)).unwrap();
+    mangled
+}
+
 pub fn mangle_function(
     name: &str,
     parameters: &[r#type::Expression],
     returns: &[r#type::Expression],
 ) -> String {
-    let mut mangled = format!("_I{}_", name.replace('_', "__"));
+    let mut mangled = format!("_I{}_", escape(name));
 
     match returns {
         [] => mangled.push('p'),
@@ -200,12 +222,21 @@ pub fn mangle_function(
 fn mangle_type(r#type: &r#type::Expression, mangled: &mut String) {
     match r#type {
         r#type::Expression::Any => panic!("[INTERNAL ERROR]: any type in IR"),
-        r#type::Expression::Class(_) => todo!(),
         r#type::Expression::Integer => mangled.push('i'),
         r#type::Expression::Boolean => mangled.push('b'),
+        r#type::Expression::Class(class) => {
+            let class = symbol::resolve(*class);
+            mangled.push('o');
+            write!(mangled, "{}", class.len()).unwrap();
+            write!(mangled, "{}", escape(class)).unwrap();
+        }
         r#type::Expression::Array(r#type) => {
             mangled.push('a');
             mangle_type(&*r#type, mangled);
         }
     }
+}
+
+fn escape(string: &str) -> String {
+    string.replace('_', "__")
 }
