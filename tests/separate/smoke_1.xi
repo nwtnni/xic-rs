eb060ac36956ce31c53543967579d58679bad9d0
Newton Ni <nwtnni@gmail.com>
2022-05-28 15:49:25 -0400

    Add first end-to-end smoke test

---
+++ tests/separate/smoke_1.xi
@@ -0,0 +1,8 @@
+use io
+use conv
+use smoke_2
+
+main(args: int[][]) {
+    print("1 = ")
+    println(unparseInt(foo()))
+}
--- tests/suite/end_to_end.rs
+++ tests/suite/end_to_end.rs
@@ -1,22 +1,30 @@
 use std::iter;
 use std::process::Command;
 
+fn compile(path: &str) -> String {
+    Command::new(env!("CARGO_BIN_EXE_xic"))
+        .arg("-d")
+        .arg("-")
+        .arg(path)
+        .output()
+        .map(|output| String::from_utf8(output.stdout))
+        .unwrap()
+        .unwrap()
+}
+
 #[test_generator::test_resources("tests/execute/*.xi")]
 pub fn end_to_end(path: &str) {
     let expected_stdout = super::execute_expected(path);
-
-    let xic = Command::new(env!("CARGO_BIN_EXE_xic"))
-        .arg("-d")
-        .arg("-")
-        .arg("--libpath")
-        .arg(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/lib"))
-        .arg(path)
-        .output()
-        .unwrap();
-    let stdout = String::from_utf8(xic.stdout)
-        .map(iter::once)
-        .map(super::execute)
-        .unwrap();
-
+    let stdout = super::execute(iter::once(compile(path)));
     pretty_assertions::assert_eq!(expected_stdout, stdout);
+}
+
+mod separate {
+    #[test]
+    fn smoke() {
+        let smoke_1 = super::compile("tests/separate/smoke_1.xi");
+        let smoke_2 = super::compile("tests/separate/smoke_2.xi");
+        let stdout = super::super::execute([smoke_1, smoke_2]);
+        insta::assert_display_snapshot!(stdout);
+    }
 }
