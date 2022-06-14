34548513f69489d2fc05f82b81aad52c91732d6e
Newton Ni <nwtnni@gmail.com>
2022-06-14 01:48:11 -0400

    Run `rustfmt` on vendored `test-generator` crate

--- vendor/test-generator/test-generator/src/lib.rs
+++ vendor/test-generator/test-generator/src/lib.rs
@@ -126,7 +126,7 @@ use self::glob::{glob, Paths};
 use quote::quote;
 use std::path::PathBuf;
 use syn::parse::{Parse, ParseStream, Result};
-use syn::{parse_macro_input, Expr, ExprLit, Ident, Lit, Token, ItemFn};
+use syn::{parse_macro_input, Expr, ExprLit, Ident, ItemFn, Lit, Token};
 
 // Form canonical name without any punctuation/delimiter or special character
 fn canonical_fn_name(s: &str) -> String {
@@ -155,13 +155,11 @@ struct MacroAttributes {
 impl Parse for MacroAttributes {
     fn parse(input: ParseStream) -> Result<Self> {
         let glob_pattern: Lit = input.parse()?;
-        if ! input.is_empty() {
+        if !input.is_empty() {
             panic!("found multiple parameters, expected one");
         }
 
-        Ok(MacroAttributes {
-            glob_pattern,
-        })
+        Ok(MacroAttributes { glob_pattern })
     }
 }
 
@@ -238,8 +236,7 @@ pub fn test_resources(attrs: TokenStream, func: TokenStream) -> TokenStream {
 
     let func_copy: proc_macro2::TokenStream = func.clone().into();
 
-    let func_ast: ItemFn = syn::parse(func)
-        .expect("failed to parse tokens as a function");
+    let func_ast: ItemFn = syn::parse(func).expect("failed to parse tokens as a function");
 
     let func_ident = func_ast.ident;
 
@@ -358,8 +355,7 @@ pub fn bench_resources(attrs: TokenStream, func: TokenStream) -> TokenStream {
 
     let func_copy: proc_macro2::TokenStream = func.clone().into();
 
-    let func_ast: ItemFn = syn::parse(func)
-        .expect("failed to parse tokens as a function");
+    let func_ast: ItemFn = syn::parse(func).expect("failed to parse tokens as a function");
 
     let func_ident = func_ast.ident;
 
@@ -402,7 +398,6 @@ pub fn bench_resources(attrs: TokenStream, func: TokenStream) -> TokenStream {
     // transforming proc_macro2::TokenStream into proc_macro::TokenStream
     result.1.into()
 }
-
 
 /// **Experimental** Helper function encapsulating and unwinding each phase, namely setup, test and teardown
 //fn run_utest<U, T, D, C>(setup: U, test: T, teardown: D) -> ()
@@ -501,7 +496,6 @@ fn concat_ts(
 ) -> proc_macro2::TokenStream {
     quote! { #accu #other }
 }
-
 
 /// Parser elements
 struct GlobExpand {
