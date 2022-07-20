5eec58b3c72b213ed8dab3e3ed23cbc5ab64328c
Newton Ni <nwtnni@gmail.com>
2022-07-20 14:19:28 -0400

    Transfer across binary operators

--- src/allocate/check.rs
+++ src/allocate/check.rs
@@ -2,8 +2,10 @@ use anyhow::anyhow;
 
 use crate::analyze::Analysis;
 use crate::data::asm;
+use crate::data::operand;
 use crate::data::operand::Register;
 use crate::data::operand::Temporary;
+use crate::util::Or;
 use crate::Map;
 
 // https://cfallin.org/blog/2021/03/15/cranelift-isel-3/
@@ -52,18 +54,21 @@ impl<const LINEAR: bool> Analysis<asm::Function<Temporary>> for ValidAllocation<
     fn transfer(&self, statement: &asm::Statement<Temporary>, output: &mut Self::Data) {
         match statement {
             asm::Statement::Label(_) | asm::Statement::Jmp(_) | asm::Statement::Jcc(_, _) => (),
-            asm::Statement::Binary(binary, operands) => match binary {
-                asm::Binary::Add => todo!(),
-                asm::Binary::Sub => todo!(),
-                asm::Binary::Mul => todo!(),
-                asm::Binary::And => todo!(),
-                asm::Binary::Or => todo!(),
-                asm::Binary::Xor => todo!(),
-                asm::Binary::Cmp => todo!(),
-                asm::Binary::Mov => todo!(),
-                asm::Binary::Lea => todo!(),
-                asm::Binary::Shl => todo!(),
-            },
+            asm::Statement::Binary(binary, operands) => {
+                let access = match binary {
+                    asm::Binary::Add
+                    | asm::Binary::Sub
+                    | asm::Binary::Mul
+                    | asm::Binary::And
+                    | asm::Binary::Or
+                    | asm::Binary::Xor
+                    | asm::Binary::Shl => BinaryAccess::R_RW,
+                    asm::Binary::Cmp => BinaryAccess::R_R,
+                    asm::Binary::Mov | asm::Binary::Lea => BinaryAccess::R_W,
+                };
+
+                self.transfer_binary(output, access, operands)
+            }
             asm::Statement::Unary(unary, operand) => match unary {
                 asm::Unary::Push => todo!(),
                 asm::Unary::Pop => todo!(),
@@ -116,7 +121,62 @@ impl<const LINEAR: bool> Analysis<asm::Function<Temporary>> for ValidAllocation<
     }
 }
 
+#[allow(non_camel_case_types)]
+enum BinaryAccess {
+    /// Read source, read and write destination
+    R_RW,
+    /// Read source, read destination
+    R_R,
+    /// Read source, write destination
+    R_W,
+}
+
 impl<const LINEAR: bool> ValidAllocation<LINEAR> {
+    fn transfer_binary(
+        &self,
+        output: &mut Map<Location, Value>,
+        access: BinaryAccess,
+        operands: &operand::Binary<Temporary>,
+    ) {
+        operands
+            .source()
+            .map(|temporary| self.read(output, temporary));
+
+        // Read destination
+        match access {
+            BinaryAccess::R_W => (),
+            BinaryAccess::R_RW | BinaryAccess::R_R => match operands.destination() {
+                Or::L(allocated) => self.read(output, &allocated),
+                Or::R(address) => {
+                    address.map(|temporary| self.read(output, temporary));
+                }
+            },
+        }
+
+        // Write destination
+        match access {
+            BinaryAccess::R_R => (),
+            BinaryAccess::R_RW | BinaryAccess::R_W => match operands.destination() {
+                Or::L(allocated) => self.write(output, allocated),
+                Or::R(_) => (),
+            },
+        }
+    }
+
+    fn read(&self, output: &Map<Location, Value>, temporary: &Temporary) {
+        let location = self.get(temporary);
+        match output.get(&location).copied().unwrap_or(Value::Unknown) {
+            Value::Conflict => panic!("Reading {} from conflict", temporary),
+            Value::Temporary(allocated) => assert_eq!(*temporary, allocated),
+            Value::Unknown => (),
+        }
+    }
+
+    fn write(&self, output: &mut Map<Location, Value>, temporary: Temporary) {
+        let location = self.get(&temporary);
+        output.insert(location, Value::Temporary(temporary));
+    }
+
     fn get(&self, temporary: &Temporary) -> Location {
         self.allocated
             .get(temporary)
@@ -125,16 +185,5 @@ impl<const LINEAR: bool> ValidAllocation<LINEAR> {
             .or_else(|| self.spilled.get(temporary).copied().map(Location::Stack))
             .ok_or_else(|| anyhow!("[INTERNAL ERROR]: unallocated temporary: {}", temporary))
             .unwrap()
-    }
-
-    fn read(&self, output: &Map<Location, Value>, temporary: &Temporary) -> Value {
-        let location = self.get(temporary);
-        output.get(&location).copied().unwrap_or(Value::Unknown)
-    }
-
-    fn write(&mut self, output: &mut Map<Location, Value>, temporary: Temporary) {
-        let location = self.get(&temporary);
-        let value = output.entry(location).or_insert(Value::Unknown);
-        *value = value.merge(&Value::Temporary(temporary));
     }
 }
